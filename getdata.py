8ab15a10a3a56ec5e00eaaf672b547d4e31fb350
abhishek nagaraj <dalek2point3@gmail.com>
2014-06-26 14:56:12 -0400

    added more fields and writing capability

--- getdata.py
+++ getdata.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import os.path
+import re
 
 def cache_data(url, fname):
 
@@ -33,17 +34,53 @@ def parse_data(soup):
 
     data = []
 
-    for row in rows:
-       ismap = 0
-       if len(row.select(".maptag")) == 0:
-           ismap = 1
 
-       data.append([row['data-pid'], ismap])
+    for row in rows:
+       ismap = "nomap"
+       ispix = "nopic"
+
+       if len(row.select(".maptag")) != 0:
+           ismap = "map"
+
+       if len(row.select("span.px")[0].select("span.p")) != 0:
+           ispix = "pic"
+
+       date = row.select("span.date")[0].text
+
+       try:
+           price = row.select("span.price")[0].text
+       except IndexError:
+           price = "NA"
+
+
+## <p class="row" data-pid="4526693657"> <a class="i" data-id="0:00P0P_i3itTi5eQsI" href="/apa/4526693657.html"></a> <span class="star"></span> <span class="pl"> <span class="date">Jun 24</span> <a href="/apa/4526693657.html">2624 Niazuma Avenue South -- Highland Park, Birmingham AL 35205</a> </span> <span class="l2"> <span class="price">$795</span> / 2br -  <span class="pnr"> <small> (Highland Avenue South)</small> <span class="px"> <span class="p"> pic<span class="maptag" data-pid="4526693657">map</span></span></span> </span> <a class="gc" data-cat="apa" href="/apa/">apts/housing for rent</a> </span> </p>
+
+
+       link = row.select("span.pl")[0].find(href=re.compile("html"))
+       href = link['href']
+
+       # title = link.text
+       try:
+           area = row.select("span.pnr")[0].find("small").text
+       except AttributeError:
+           area = "NA"
+
+       gc = row.select("a.gc")[0].text
+
+       data.append([row['data-pid'], ismap, ispix, date, price, gc, href, area])
            
     return data
 
-def write_data():
-    pass
+def write_data(data, outfile, subdomain):
+    
+    with open(outfile, "w") as f:
+        for item in data:
+            item.append(subdomain)
+            item = [x.strip("\t") for x in item]
+            item = [x.strip(",") for x in item]
+            line = "\t".join(x.encode('utf-8') for x in item) + "\n"
+            
+            f.write(line)
 
 def main():
     
@@ -53,7 +90,7 @@ def main():
     directory = "data/"
     fname_stub = "-".join([subdomain, section, str(step)])
     ext = ".html"
-    outfile = ""
+    outfile = "test.tsv"
 
     url = "http://" + subdomain + ".craigslist.org/" + section + "/index" + str(step) + ".html#list"
     fname = directory + fname_stub + ext
@@ -61,7 +98,7 @@ def main():
     # cache_data(url, fname)
     soup = load_data(fname)
     data = parse_data(soup)
-    write_data(data)
+    write_data(data, outfile, subdomain)
 
 if __name__ == "__main__":
 
